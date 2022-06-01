8b56b59b6972420f502b3da1857f3abae9ae917f
Erick Recher <deathofasellout@yahoo.com>
2022-06-01 11:21:37 -0700

    finished

--- classes/student.js
+++ classes/student.js
@@ -1,5 +1,23 @@
 const Person = require('./person');
 
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA){
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+    //Check the specs to determine which parameters you need to inherit.
+  }
+  static compareGPA(firstSt, secondSt) {
+    if (firstSt.GPA > secondSt.GPA) {
+      return `${firstSt.firstName} ${firstSt.lastName} has the higher GPA.`;
+    } else if (firstSt.GPA < secondSt.GPA) {
+      return `${secondSt.firstName} ${secondSt.lastName} has the higher GPA.`;
+    } else {
+      return "Both students have the same GPA.";
+    }
+  }
+}
+
 // Your code here
 
 /****************************************************************************/
--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,22 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience ){
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+  static combinedYearsOfExperience(teachers){
+    let total = 0;
+    teachers.forEach(teacher => total += teacher.yearsOfExperience);
+    return total;
+  }
+}
+
+
+// Create a static method called combinedYearsOfExperience that takes in an array
+// of Teacher instances, teachers, as a parameter and returns the sum of the
+// yearsOfExperience of all teachers.
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
